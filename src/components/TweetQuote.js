6c548b05fb6953019abf08674ee3d01afca7c805
unknown <samuel_newhouse@yahoo.com>
2019-04-12 17:14:39 -0500

    Add svg and hovertip to <TweetQuote>

--- src/components/TweetQuote.js
+++ src/components/TweetQuote.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import twitterLogo from '../images/twitter-logo.svg'
 
 const TweetQuote = ({ author, quote }) => {
   const textURI = encodeURIComponent('"' + quote + '" - ' + author);
   const tweetURL = "https://twitter.com/intent/tweet?text=" + textURI;
 
   return (
-    <a id="tweet-quote" href={tweetURL} target="_blank" rel="noopener noreferrer">Tweet</a>
+    <a className="hovertip btn btn-dark" id="tweet-quote" href={tweetURL} target="_blank" rel="noopener noreferrer">
+      <img src={twitterLogo} alt="Tweet this quote."></img>
+      <span className="hovertiptext">Tweet this quote.</span>
+    </a>
   )
 }
 
