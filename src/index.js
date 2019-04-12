ba5a357f3b8ec443fc39a52e25c27e409d8febd8
unknown <samuel_newhouse@yahoo.com>
2019-04-12 17:11:59 -0500

    Remove <App> and serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import QuoteBox from './components/QuoteBox';
 import handleActions from "./reducers";
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
@@ -12,12 +10,7 @@ const store = createStore(handleActions, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <QuoteBox />
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
