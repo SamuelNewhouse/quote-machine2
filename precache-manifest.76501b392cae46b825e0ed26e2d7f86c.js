33ccd41a0b71db8b1afb38fa679836e08d271c3c
unknown <samuel_newhouse@yahoo.com>
2019-04-16 09:31:51 -0500

    Updates

---
+++ precache-manifest.76501b392cae46b825e0ed26e2d7f86c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d498cedda0e2aeb3015881d30bf509ac",
+    "url": "./static/media/twitter-logo.d498cedd.svg"
+  },
+  {
+    "revision": "72232123559ab11e06123df525d3cb32",
+    "url": "./static/media/google-logo.72232123.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b0fa5aae48c1469c682f",
+    "url": "./static/js/main.b9f09f19.chunk.js"
+  },
+  {
+    "revision": "6756f71e707d4e3d6ca9",
+    "url": "./static/js/2.4ed65eaa.chunk.js"
+  },
+  {
+    "revision": "b0fa5aae48c1469c682f",
+    "url": "./static/css/main.47b61cbd.chunk.css"
+  },
+  {
+    "revision": "6756f71e707d4e3d6ca9",
+    "url": "./static/css/2.5c607276.chunk.css"
+  },
+  {
+    "revision": "7464eb967dbc3f01d8489923d5a6ae19",
+    "url": "./index.html"
+  }
+];
