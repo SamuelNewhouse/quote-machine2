65d932ff5ff096f451e43238e559addc34737f75
unknown <samuel_newhouse@yahoo.com>
2019-07-03 12:51:18 -0500

    Updates

---
+++ precache-manifest.e0f69219a7e14cc881632fde30d28bdd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f437008e80266192ead38c3785f933",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8ad32b396d269e5b95da",
+    "url": "./static/css/2.545c6387.chunk.css"
+  },
+  {
+    "revision": "6f97aaa979189e09a81f",
+    "url": "./static/css/main.942b4f2e.chunk.css"
+  },
+  {
+    "revision": "8ad32b396d269e5b95da",
+    "url": "./static/js/2.504abbd4.chunk.js"
+  },
+  {
+    "revision": "6f97aaa979189e09a81f",
+    "url": "./static/js/main.4c42e04c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "72232123559ab11e06123df525d3cb32",
+    "url": "./static/media/google-logo.72232123.svg"
+  },
+  {
+    "revision": "d498cedda0e2aeb3015881d30bf509ac",
+    "url": "./static/media/twitter-logo.d498cedd.svg"
+  }
+]);
